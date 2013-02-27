11c9df0633efcf1f19693f96040ae5e0065b9248
Leon Timmermans <fawaka@gmail.com>
2013-02-27 21:42:59 +0100

    Refactored option gathering

--- lib/Dist/Build/Loader.pm
+++ lib/Dist/Build/Loader.pm
@@ -11,17 +11,6 @@ use Getopt::Long qw/GetOptionsFromArray/;
 use JSON 2 qw/decode_json/;
 use Module::Runtime qw/require_module/;
 
-has options => (
-	isa      => 'ArrayRef[Str]',
-	traits   => ['Array'],
-	init_arg => undef,
-	default  => sub { [qw/config=s% verbose:1/] },
-	handles  => {
-		add_options => 'push',
-		options     => 'elements',
-	},
-);
-
 has info_class => (
 	is      => 'ro',
 	isa     => 'Str',
@@ -47,13 +36,14 @@ sub create_builder {
 	my ($self, $meta, $args, $env) = @_;
 	my $pregraph   = decode_json(read_file(q{_build/graph}));
 	my $commandset = Build::Graph::CommandSet->new;
+	my @options    = qw/config=s% verbose:1/;
 	for my $dependency (@{ $pregraph->{dependencies} }) {
 		my $module = $self->_load_plugin($dependency);
 		my $plugin = $module->new(name => $dependency);
 		$plugin->configure_commands($commandset) if $plugin->does('Build::Graph::Role::Command');
-		$self->add_options($plugin->options) if $plugin->does('Dist::Build::Role::OptionProvider');
+		push @options, $plugin->options if $plugin->does('Dist::Build::Role::OptionProvider');
 	}
-	my ($action, $opt, $config) = $self->_parse_arguments($args, $env);
+	my ($action, $opt, $config) = $self->_parse_arguments($args, $env, \@options);
 	my $graph = Build::Graph->new(commands => $commandset, info_class => $self->info_class);
 	$graph->load_from_hashref($pregraph->{graph});
 	require Dist::Build::Builder;
@@ -78,13 +68,13 @@ sub create_configurator {
 }
 
 sub _parse_arguments {
-	my ($self, $args, $env) = @_;
+	my ($self, $args, $env, $options) = @_;
 	my $bpl     = decode_json(read_file('_build/params'));
 	my $action  = @{$args} && $args->[0] =~ / \A \w+ \z /xms ? shift @{$args} : 'build';
 	my $rc_opts = read_config();
 	my @env     = defined $env->{PERL_MB_OPT} ? split_like_shell($env->{PERL_MB_OPT}) : ();
 	my @all     = map { @{$_} } grep { defined } $rc_opts->{'*'}, $bpl, $rc_opts->{$action}, \@env, $args;
-	GetOptionsFromArray(\@all, \my %opt, $self->options);
+	GetOptionsFromArray(\@all, \my %opt, @{$options});
 	my $config = ExtUtils::Config->new(delete $opt{config});
 	return ($action, \%opt, $config);
 }
