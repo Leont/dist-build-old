3ef6b7b90a89fc5f6eb0135b18c20bb4eb8b0198
Leon Timmermans <fawaka@gmail.com>
2015-02-26 01:12:07 +0100

    Added script support to CopyPM

--- lib/Dist/Build/Plugin/CopyPM.pm
+++ lib/Dist/Build/Plugin/CopyPM.pm
@@ -3,25 +3,40 @@ package Dist::Build::Plugin::CopyPM;
 use strict;
 use warnings;
 
-use parent qw/Build::Graph::Role::Plugin Dist::Build::Role::Manipulator/;
+use parent qw/Build::Graph::Role::CommandProvider Dist::Build::Role::Manipulator/;
 
 use File::Spec::Functions qw/catfile/;
 
-sub dependencies {
-	return 'Dist::Build::Plugin::Sanity';
+sub _get_commands {
+	return {
+		make_executable => sub {
+			my $info = shift;
+			my $target = $info->name;
+			require ExtUtils::Helpers;
+			ExtUtils::Helpers::make_executable($target);
+		},
+		pl_to_blib => [ 'Core/copy', 'CopyPM/make_executable' ],
+	};
 }
 
 sub manipulate_graph {
 	my ($self, $graph) = @_;
 
 	$graph->add_phony('copy_pm', dependencies => ['$(pm-blib)']);
-	$graph->get_node('build')->add_dependencies('copy_pm');
+	$graph->add_phony('copy_pl', dependencies => ['$(pl-blib)']);
+	$graph->get_node('build')->add_dependencies('copy_pm', 'copy_pl');
 
 	my $pms = $graph->add_wildcard(dir => 'lib', pattern => '*.{pm,pod}', name => 'pm-files');
 	$graph->add_subst($pms,
 		subst  => sub { my $source = shift; catfile('blib', $source) },
 		action => sub { my ($target, $source) = @_; [ 'Core/copy', $source ] },
 		name   => 'pm-blib',
+	);
+	my $pls = $graph->add_wildcard(dir => 'script', pattern => '*', name => 'pl-files');
+	$graph->add_subst($pls,
+		subst  => sub { my $source = shift; catfile('blib', $source) },
+		action => sub { my ($target, $source) = @_; [ 'CopyPM/pl_to_blib', $source ] },
+		name   => 'pl-blib',
 	);
 	return;
 }
