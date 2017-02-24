1d8ed6e0961b407f9895d347d5c5eeb09e867d8e
Leon Timmermans <fawaka@gmail.com>
2017-02-24 18:05:35 +0100

    Delay execution of callback

--- lib/Dist/Build/Role/CommandSet.pm
+++ lib/Dist/Build/Role/CommandSet.pm
@@ -1,10 +1,14 @@
 package Dist::Build::Role::CommandSet;
 
+use strict;
+use warnings;
 use base 'Build::Graph::Role::CommandSet';
 
 sub get_action {
-	my ($self, $name) = @_;
-	return $self->get_actions($self->{graph})->{$name} || Carp::croak("No such command $name in $self->{name}");
+	my ($self, $name, @arguments) = @_;
+	my $action = $self->get_actions($self->{graph})->{$name};
+	Carp::croak("No such command $name in $self->{name}") if not $action;
+	return sub { $action->(@arguments) };
 }
 
 sub get_actions {
@@ -12,8 +16,10 @@ sub get_actions {
 }
 
 sub get_transformation {
-	my ($self, $name) = @_;
-	return $self->get_trans($self->{graph})->{$name} || Carp::croak("No such transformation $name in $self->{name}");
+	my ($self, $name, @arguments) = @_;
+	my $trans = $self->get_trans($self->{graph})->{$name};
+	Carp::croak("No such transformation $name in $self->{name}") if not $trans;
+	return sub { $trans->(@arguments) };
 }
 
 sub get_trans {
