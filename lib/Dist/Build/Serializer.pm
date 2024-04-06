9012ec83dcdab4a4731a4217395a54080ca57df2
Leon Timmermans <fawaka@gmail.com>
2024-04-06 22:18:15 +0200

    Initial code

---
+++ lib/Dist/Build/Serializer.pm
@@ -0,0 +1,50 @@
+package Dist::Build::Serializer;
+
+use strict;
+use warnings;
+
+use parent 'ExtUtils::Builder::Serializer';
+
+use List::Util 'any';
+
+use ExtUtils::Builder::Action::Function;
+use Dist::Build::Core;
+
+sub serialize_action {
+	my ($self, $action, %args) = @_;
+
+	if ($action->isa('ExtUtils::Builder::Action::Function') && $action->module eq 'Dist::Build::Core') {
+		return [ $action->function, $action->arguments ];
+	} else {
+		return $self->SUPER::serialize_action($action);
+	}
+}
+
+sub deserialize_action {
+	my ($self, $serialized, %options) = @_;
+	my ($command, @args) = @{$serialized};
+
+	if (any { $command eq $_ } @Dist::Build::Core::EXPORT_OK) {
+		return make_function($command, @args);
+	} else {
+		$self->SUPER::deserialize_action($serialized, %options);
+	}
+}
+
+sub make_function {
+	my ($command, @args) = @_;
+	ExtUtils::Builder::Action::Function->new(
+		function  => $command,
+		module    => 'Dist::Build::Core',
+		arguments => \@args,
+		exports   => 'explicit',
+	);
+}
+
+1;
+
+# ABSTRACT: A Serializer for a Dist::Build plan
+
+=head1 DESCRIPTION
+
+This is a subclass of L<ExtUtils::Builder::Serializer|ExtUtils::Builder::Serializer> that optimizes the serialization of C<Dist::Build> actions such as those in L<Dist::Build::Core>.
